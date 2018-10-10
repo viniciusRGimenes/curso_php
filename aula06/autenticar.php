38ac6ec66b64737da8b722c0a0431e917cfdc99d
Almir Farias Rivas Junior <rivasalmir@gmail.com>
2018-10-09 22:02:18 -0300

    Refatorar login

--- aula06/autenticar.php
+++ aula06/autenticar.php
@@ -1,23 +1,16 @@
 <?php
 
+require_once("user.php");
+
+// Receber dados enviados
 $email = $_POST["email"];
 $password = $_POST["senha"];
 
-$db_server = "curso_php_db_1";
-$db_user = "root";
-$db_password = "phprs";
-$db_database = "academia";
-
-$conn = new mysqli($db_server, $db_user, $db_password, $db_database);
-if($conn->connect_error){
-    die("Erro ao tentar conectar no banco de dados: ". $conn->connect_error);
-}
-
-$sql = "SELECT * FROM users WHERE email = '".$email."' AND senha = '".$password."'";
-$result = $conn->query($sql);
-
-if($result->num_rows > 0){
+$user = new User();
+$usuario_valido = $user->autenticar_usuario($email, $password); 
+if($usuario_valido){
     header("location:home.php");
 }else{
     header("location:login.php?erro=1");
 }
+
